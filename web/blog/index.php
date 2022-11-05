eb97e2a467f731e043e672458df551b12f6502b5
Kouichi_Takano <tx2bxn@icloud.com>
2022-11-05 21:14:57 +0900

    layout

--- web/blog/index.php
+++ web/blog/index.php
@@ -4,7 +4,16 @@ include('../../app/_parts/_header.php');
 $id = $_GET['id'];
 ?>
 
-<?php echo GetContent("blog", $id)['title'] ?>
+<!-- html start -->
+<div class="container">
+  <div class="row">
+    <div class="col mt-3">
+      <img src="<?php echo GetContent("blog", $id)['eyecatch']['url']; ?>" class="" alt="">
+      <h2 class="fs-5 mb-1 pb-1 border-bottom"><?php echo GetContent("blog", $id)['title'] ?></h2>
+    </div>
+  </div>
+</div>
+<!-- html end -->
 
 <?php
 include('../../app/_parts/_footer.php');
